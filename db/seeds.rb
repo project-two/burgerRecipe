1b0a1e59e922e6874f1fac49881d2f9350d31a43
danielq <qiaoer0505@gmail.com>
2019-12-12 18:48:46 +1100

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-User.destroy_all 
-
-include BCrypt
-
 User.create(
     name:'kay',
     email: 'kay.bot@ga.com.au',
-    password_digest: Password.create('12345'),
+    password: '12345',
     admin: true,
 );
 
 User.create(
     name: 'kyle',
     email: 'kyle@ga.com.au',
-    password_digest: Password.create('kyle_1234'),
+    password: 'kyle_1234',
     admin: true, 
 )
 User.create(
     name: 'daniel',
     email: 'daniel@ga.com.au',
-    password_digest: Password.create('daniel_1234'),
+    password: 'daniel_1234',
     admin: true, 
 )
 
-puts "seeding data base created #{User.all.length}"
+puts "seeding User, created #{User.all.length}"
