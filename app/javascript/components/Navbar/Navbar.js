ffa920d50ed3b4eba24f1a27e4d5da152b76ff9a
“kylemharman” <kylemartinharman@gmail.com>
2019-12-14 17:12:17 +1100

    add changes

--- app/javascript/components/Navbar/Navbar.js
+++ app/javascript/components/Navbar/Navbar.js
@@ -16,24 +16,21 @@ class Navbar extends Component {
           <StyledLink to="/">
             <i className="fas fa-hamburger fa-2x"></i>
           </StyledLink>
-          <StyledLink to="/user/:user_id">My Recipes</StyledLink>
         </LinkContainer>
         {!user.isLoggedIn ? (
           <div>
             <StyledLink to="/login">Login</StyledLink>
             <StyledLink to="/sign-up">Sign Up</StyledLink>
-            <StyledLink to="/new-recipe">Add new Recipe</StyledLink>
           </div>
         ) : null}
-        {user.isLoggedIn && user.currentUser ? (
-          <React.Fragment>
-            {user.currentUser.name}
-            <br />
-            <a href="#" onClick={(e) => this.handleLogoutClick(e)}>
-              logout
-            </a>
-          </React.Fragment>
-        ) : null}
+        { user.isLoggedIn && user.currentUser ?
+            <React.Fragment>
+                <StyledLink to="/user/:user_id">My Recipes</StyledLink>
+                <StyledLink to="/new-recipe">Add new Recipe</StyledLink>
+                <StyledLink to='' onClick={(e)=>this.handleLogoutClick(e)}>{user.currentUser.name} logout</StyledLink>
+            </React.Fragment>
+            :null
+        }
       </NavBarContainer>
     );
   }
