b18bd7be051845e7d933fe92b04c2ceca4ceb364
KAY S <KS.KWANRUAN@GMAIL.com>
2019-12-17 20:50:30 +1100

    Post recipe to endpoint implemented

--- app/javascript/pages/CreateRecipe/CreateRecipe.js
+++ app/javascript/pages/CreateRecipe/CreateRecipe.js
@@ -6,24 +6,21 @@ import axios from "axios";
 const CreateRecipe = () => {
   const handleCreateRecipe = (e, data) => {
     e.preventDefault();
-    console.log("I am called");
-    console.log(data);
-    //e.preventDefault(e);
-    //   axios
-    //     .post(`/to-be-changed`,{
-    //       recipe: {
-    //         burgerName: data.burgerName,
-    //         image: data.image,
-    //         ingredients: data.ingredients,
-    //         method: data.method
-    //       }
-    //     })
-    //     .then((response) => {
-    //       // TODO: If have time, ToastContainer to be used here
-    //       alert("recipe successfully created");
-    //       // Navigate to the home page.
-    //       navigate("/user/:user_id");
-    //     });
+    axios
+      .post(`/api/recipes`, {
+        recipe: {
+          burgerName: data.burgerName,
+          image: data.image,
+          ingredients: data.ingredients,
+          method: data.method
+        }
+      })
+      .then((response) => {
+        // TODO: If have time, ToastContainer to be used here
+        alert("recipe successfully created");
+        // Navigate to the home page.
+        navigate("/user/:user_id");
+      });
   };
 
   return <CreateRecipeForm onAddRecipeClick={handleCreateRecipe} />;
