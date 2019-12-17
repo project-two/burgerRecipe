7e7851ba43885b179eee40d09c53e75d4b43a78d
danielq <qiaoer0505@gmail.com>
2019-12-17 19:16:19 +1100

    add details on like_recipe returns

--- app/controllers/api/like_recipes_controller.rb
+++ app/controllers/api/like_recipes_controller.rb
@@ -6,12 +6,22 @@ class Api::LikeRecipesController < ApplicationController
       else
         render json: LikeRecipe.count, status: :ok
       end
-    elsif request.query_parameters.key?("recipe")
-      render json: LikeRecipe.where("recipe_id": request.query_parameters["recipe"]), status: :ok
-    elsif request.query_parameters.key?("user")
-      render json: LikeRecipe.where("user_id": request.query_parameters["user"]), status: :ok
     else
-      render json: LikeRecipe.all, status: :ok
+      recipes = LikeRecipe.all
+      if request.query_parameters.key?("recipe")
+        recipes = recipes.where("recipe_id": request.query_parameters["recipe"])
+      elsif request.query_parameters.key?("user")
+        recipes = recipes.where("user_id": request.query_parameters["user"])
+      end
+      recipes_json = recipes.as_json
+      recipes_json.each do |recipe|
+        recipe["username"] = User.find(recipe["user_id"]).name
+        recipe_obj = Recipe.find(recipe["recipe_id"])
+        recipe["name"] = recipe_obj.name
+        recipe["url"] = recipe_obj.url
+        recipe["like_count"] = recipe_obj.like_recipes.count
+      end
+      render json: recipes_json, status: :ok
     end
   end
 
