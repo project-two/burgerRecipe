e01aa76c1d5b16a059aac09b7ca6c95e217afb79
danielq <qiaoer0505@gmail.com>
2019-12-17 18:49:14 +1100

    add username and like_count in returned recipe json

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,5 +1,10 @@
 class Api::RecipesController < ApplicationController
   def index
-    render json: Recipe.all, status: :ok
+    recipes_json = Recipe.all.as_json
+    recipes_json.each do |recipe|
+      recipe["username"] = User.find(recipe["user_id"]).name
+      recipe["like_count"] = Recipe.find(recipe["id"]).like_recipes.count
+    end
+    render json: recipes_json, status: :ok
   end
 end
