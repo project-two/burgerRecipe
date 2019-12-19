0041ae6eca2cf45cca3703bf96aa7a80132159bd
danielq <qiaoer0505@gmail.com>
2019-12-19 21:16:05 +1100

    front end finished for post recipe

--- app/javascript/pages/CreateRecipe/CreateRecipe.js
+++ app/javascript/pages/CreateRecipe/CreateRecipe.js
@@ -6,14 +6,12 @@ class CreateRecipe extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      recipe: {
-        name: "",
-      },
+      recipe: "",
       instructions: [''],
       ingredients: [
         {
           qty: "",
-          unit: "",
+          unit: "spoon",
           name: ""
         }
       ]
@@ -24,7 +22,7 @@ class CreateRecipe extends React.Component {
     let ingrediends = this.state.ingredients;
     ingrediends.push({
       qty: "",
-      unit: "",
+      unit: "spoon",
       name: ""
     });
     this.setState({
@@ -61,8 +59,47 @@ class CreateRecipe extends React.Component {
   };
 
   handleSubmit = e => {
-    console.log(e)
+    const user_id = JSON.parse(sessionStorage.getItem("auth")).userId;
+    console.log(this.state)
+    let state = this.state
+    state["user_id"] = user_id
+    axios.post(`/api/recipes`, { state })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
     e.preventDefault();
+  }
+
+  handleChange = event => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name.split('_');
+    let state = this.state
+    console.log(name, value)
+    console.log(target.order)
+    if (name[0] === "recipe") {
+      this.setState({
+        [name[0]]: value
+      });
+    } else if (name[0] === "ingrd") {
+      if (name[1] === "qty") {
+        state.ingredients[name[2]].qty = value
+      } else if (name[1] === "unit") {
+        state.ingredients[name[2]].unit = value
+      } else {
+        state.ingredients[name[2]].name = value
+      }
+      this.setState({
+        ingredients: state.ingredients
+      });
+    } else if (name[0] === "step") {
+      state.instructions[name[1]] = value
+      this.setState({
+        instructions: state.instructions
+      });
+    }
+    
   }
 
   render() {
@@ -73,7 +110,7 @@ class CreateRecipe extends React.Component {
         <div>This is create Recipe Page</div>
         <form onSubmit={this.handleSubmit}>
           <div>Name</div>
-          <input type="text" name="name" />
+          <input type="text" name="recipe" onChange={this.handleChange}/>
           <div>Ingredients</div>
           <table>
             <thead>
@@ -88,19 +125,19 @@ class CreateRecipe extends React.Component {
               {ingredients.map((ingredient, idx) => {
                 return (
                   <tr key={`ingrd_${idx}`}>
-                    <td>{idx}</td>
+                    <td>{idx+1}</td>
                     <td>
-                      <input type="text" name={`ingrd_qty_${idx}`} />
+                      <input type="text" name={`ingrd_qty_${idx}`} onChange={this.handleChange}/>
                     </td>
                     <td>
-                      <select>
+                      <select name={`ingrd_unit_${idx}`} value="spoon" onChange={this.handleChange}>
                         <option value="spoon">spoon</option>
                         <option value="gram">gram</option>
                         <option value="whatever">whatever</option>
                       </select>
                     </td>
                     <td>
-                      <input type="text" name={`ingrd_unit_${idx}`} />
+                      <input type="text" name={`ingrd_text_${idx}`} onChange={this.handleChange}/>
                     </td>
                   </tr>
                 );
@@ -120,10 +157,10 @@ class CreateRecipe extends React.Component {
             <tbody>
               {instructions.map((step, idx) => {
                 return (
-                  <tr key={`step_${idx}`}>
+                  <tr key={`step_${idx+1}`}>
                     <td>{idx}</td>
                     <td>
-                    <input type="text" name={`step_${idx}`} />
+                    <input type="text" name={`step_${idx}`} onChange={this.handleChange}/>
                     </td>
                   </tr>
                 )
